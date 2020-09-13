d9485ba90f5ffc73a8123535e5c6043910eb8349
Ian <iangu234@gmail.com>
2020-09-12 20:31:14 -0400

    Updates

---
+++ precache-manifest.98860c5820b64a93f8960a40b3a7dfe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95b43165439b1796760a483c9119852",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "4c2899f4d77653345b27",
+    "url": "/blog/static/css/main.6d9b8b8e.chunk.css"
+  },
+  {
+    "revision": "f392f3f309607f28f21d",
+    "url": "/blog/static/js/2.e2808f33.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/blog/static/js/2.e2808f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2899f4d77653345b27",
+    "url": "/blog/static/js/main.b899d683.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  },
+  {
+    "revision": "5097b0247a92d47178df598b82944f15",
+    "url": "/blog/static/media/default-avatar.5097b024.png"
+  }
+]);
